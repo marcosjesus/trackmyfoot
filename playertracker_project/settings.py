4523636c99f0c3e68855ae36abb56262d799f966
marcosjesus <marcos.luiz.jesus@hotmail.com>
2025-04-30 10:56:08 -0400

    adicionado static_root

--- playertracker_project/settings.py
+++ playertracker_project/settings.py
@@ -91,6 +91,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dashboard/static')]
 
 MEDIA_URL = '/media/'
