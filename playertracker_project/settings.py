b3567f540cdcca9f1df83b22f9e9c46975886068
marcosjesus <marcos.luiz.jesus@hotmail.com>
2025-05-01 08:43:53 -0400

    correcao de UTC = False

--- playertracker_project/settings.py
+++ playertracker_project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
