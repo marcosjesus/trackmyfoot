8ad1f21ce61c924d361a396b4b70b3615aa49fd9
marcosjesus <marcos.luiz.jesus@hotmail.com>
2025-05-02 06:58:59 -0400

    Separando settings local e servidor

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playertracker_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'playertracker_project.settings.local'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ playertracker_project/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['www.trackmyfoot.com', 'trackmyfoot.com', '3.22.27.69']
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
