eba8911fbd247ea2f6d8523e1ce82f02a0fe85f9
marcosjesus <marcos.luiz.jesus@hotmail.com>
2025-05-02 11:26:03 -0400

    teste de UTC False

--- playertracker_project/settings/base.py
+++ playertracker_project/settings/base.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'playertracker_project.wsgi.application'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
